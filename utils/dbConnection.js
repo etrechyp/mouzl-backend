5ae1bc9ab01a11ed62cd4ba4a63671c9877202dd
Emiro Trechy <etrechy@lamar.local>
2024-05-06 16:33:31 -0400

    update .env

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
+const { DB_NAME, DB_USER, DB_PASS, DB_HOST}  = process.env
 
-const sequelize = new Sequelize('test', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
     dialect: 'mysql',
 });
 
